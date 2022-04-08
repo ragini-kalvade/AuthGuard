e6a43d794c236836a8a9c75abb59aeb7f2af3ba1
Vanshika Agrawal <vanshikaagrawal56@gmail.com>
2022-04-08 22:06:39 +0530

    completed signature verification

--- signature/views.py
+++ signature/views.py
@@ -1,46 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import os
 import numpy as np
 import skimage
 import cv2
-#import measure
 import metrics 
-from django.core.files.images import ImageFile
-from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.contrib import messages
 
-# Create your views here.pip
-# 
+def remove_white_space(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (25,25), 0)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    noise_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, noise_kernel, iterations=2)
+    close_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
+    close = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, close_kernel, iterations=3)
+    #Find enclosing boundingbox and crop ROI\n”,
+    coords = cv2.findNonZero(close)
+    x,y,w,h = cv2.boundingRect(coords)
+    return image[y:y+h, x:x+w]
+
 def signVerify(request):
     #print(request.FILES)
     if request.method=='POST' and request.FILES['signature_v']:
         username = None
         if request.user.is_authenticated:
             username = request.user.username
-        #print(username)
+        
         originalsignaturename = username + '_sign.jpg'
-        #print(originalsignaturename)
-        path_to_image = 'media/' + originalsignaturename
-        signature_u=InMemoryUploadedFile(open(path_to_image), None, None, None, None, None).read()
-        npimg = np.fromstring(signature_u, np.uint8)
-        userSignature = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
-        #userSignature = InMemoryUploadedFile(open(path_to_image), None, None, None, None, None)
-        print(type(userSignature))
-        #print(userSignature)
-        print(type(request.FILES['signature_v']))
+        path_to_image = './' + 'media\\' + originalsignaturename
+        #print(path_to_image)
+
+        userSignature = cv2.imread(path_to_image)
+      
         signature_v = request.FILES['signature_v'].read()
         npimg = np.fromstring(signature_v, np.uint8)
         verifySignature = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
-        print(type(verifySignature))
+
         #print(verifySignature)
         i = cv2.cvtColor(verifySignature,cv2.COLOR_BGR2RGB)
         verifySignature = cv2.resize(cv2.cvtColor(i.copy(),cv2.COLOR_BGR2GRAY),(100,100))
         userSignature = cv2.resize(cv2.cvtColor(userSignature,cv2.COLOR_BGR2GRAY),(100,100))
     
-        SSIM_Value = print(metrics.structural_similarity(userSignature,verifySignature))
-        print(SSIM_Value)
+        SSIM_Value = skimage.metrics.structural_similarity(userSignature,verifySignature)
+        
+        if(SSIM_Value < 0.8):
+            answer = 'Signature not matched'
+        else:  
+            answer = 'Signature matched'
+        messages.info(request, answer)
         return render(request, 'signature.html')
     else:
-        print('error')
-        print(request)
-        #print(request.FILES['signature_v'])
         return render(request, 'signature.html')
